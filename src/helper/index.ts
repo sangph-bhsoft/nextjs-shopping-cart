4ba04e725e68a06fa448e492ebdbb2680975686d
meocon200890 <meocon200890@gmail.com>
2020-12-14 09:14:33 +0700

    save carts to localStorage

--- src/helper/index.ts
+++ src/helper/index.ts
@@ -1,3 +1,26 @@
+import { CartItem } from '../types';
+
 export const usdCurrency = (num: number = 0) => {
   return new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(num);
+};
+export const getListCartFromLocalStorage = (): CartItem[] => {
+  if (typeof window !== 'undefined') {
+    const listCart = localStorage.getItem('carts');
+    if (listCart) return JSON.parse(listCart);
+    return [];
+  }
+  return [];
+};
+
+export const setListCartToLocalStorage = (carts: CartItem[]) => {
+  if (typeof window !== 'undefined') {
+    localStorage.removeItem('carts');
+    localStorage.setItem('carts', JSON.stringify(carts));
+  }
+};
+
+export const clearCartFromLocalStorage = () => {
+  if (typeof window !== 'undefined') {
+    localStorage.removeItem('carts');
+  }
 };
--- src/redux/reducers/cart.ts
+++ src/redux/reducers/cart.ts
@@ -1,51 +1,47 @@
 import { createReducer } from 'typesafe-actions';
+import { clearCartFromLocalStorage, getListCartFromLocalStorage, setListCartToLocalStorage } from '../../helper/index';
+
 import { ADD_TO_CART, CartActionType, REMOVE_FROM_CART, CLEAR_CART } from '../actions';
 import { CartItem, CartState } from '../../types';
 
 const INITIAL_STATE: CartState = {
-  carts: [],
+  carts: getListCartFromLocalStorage(),
 };
 
 const cartReducer = createReducer<CartState, CartActionType>(INITIAL_STATE, {
   [ADD_TO_CART]: (state, action) => {
     const { product, quantity } = action.payload;
     const index = state.carts.findIndex((item) => item.product.id === product.id);
+    const newState = { ...state };
     if (index !== -1) {
-      return {
-        ...state,
-        carts: state.carts.map((item: CartItem) =>
-          item.product.id === product.id
-            ? {
-                ...item,
-                quantity: item.quantity + quantity,
-                totalPrice: item.totalPrice + quantity * item.product.price,
-              }
-            : { ...item },
-        ),
+      newState.carts = newState.carts.map((item: CartItem) =>
+        item.product.id === product.id
+          ? {
+              ...item,
+              quantity: item.quantity + quantity,
+              totalPrice: item.totalPrice + quantity * item.product.price,
+            }
+          : { ...item },
+      );
+    } else {
+      const cartItem: CartItem = {
+        product,
+        quantity,
+        totalPrice: product.price * quantity,
       };
+      newState.carts = [...newState.carts, cartItem];
     }
-
-    const cartItem: CartItem = {
-      product,
-      quantity,
-      totalPrice: product.price * quantity,
-    };
-    return {
-      ...state,
-      carts: [...state.carts, cartItem],
-    };
+    setListCartToLocalStorage(newState.carts);
+    return newState;
   },
   [REMOVE_FROM_CART]: (state, action) => {
     const { product, quantity } = action.payload;
-    if (!quantity) {
-      return {
-        ...state,
-        carts: state.carts.filter((item) => item.product.id !== product.id),
-      };
-    }
-    return {
-      ...state,
-      carts: state.carts.map((item: CartItem) =>
+    const cartItem: CartItem = state.carts.find((item) => item.product.id === product.id);
+    const newState = { ...state };
+    if (!quantity || cartItem.quantity === 1) {
+      newState.carts = newState.carts.filter((item) => item.product.id !== product.id);
+    } else {
+      newState.carts = state.carts.map((item: CartItem) =>
         item.product.id === product.id
           ? {
               ...item,
@@ -53,10 +49,13 @@ const cartReducer = createReducer<CartState, CartActionType>(INITIAL_STATE, {
               totalPrice: item.totalPrice + quantity - item.product.price,
             }
           : { ...item },
-      ),
-    };
+      );
+    }
+    setListCartToLocalStorage(newState.carts);
+    return newState;
   },
   [CLEAR_CART]: () => {
+    clearCartFromLocalStorage();
     return INITIAL_STATE;
   },
 });
